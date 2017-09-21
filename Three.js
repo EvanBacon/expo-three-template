7c4b0484c48914447e18efa0b52ad7c587ad7fbc
Evan Bacon <baconbrix@gmail.com>
2017-09-21 14:07:40 -0500

    Init

---
+++ Three.js
@@ -0,0 +1,15 @@
+import * as THREE from 'three';
+global.THREE = THREE;
+export default THREE;
+
+if (!console.time) {
+    console.time = () => { };
+}
+if (!console.timeEnd) {
+    console.timeEnd = () => { };
+}
+
+console.ignoredYellowBox = [
+    'THREE.WebGLRenderer',
+    'THREE.WebGLProgram',
+];
