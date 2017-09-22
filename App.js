5dc224dae82f7af6cfa9a96f3ef1b5e14c2baeaf
Evan Bacon <baconbrix@gmail.com>
2017-09-22 11:37:21 -0500

    Added Support For ARKit
    • Implemented new functions from expo-three
    • Added example methods for AR Components

--- App.js
+++ App.js
@@ -17,54 +17,48 @@ class App extends React.Component {
       style={{ flex: 1 }}
       onContextCreate={this._onContextCreate}
       render={this._animate}
+      enableAR={true}
     />
   );
-  //render={_=> {}} to disable loop
 
-  _onContextCreate = async (gl) => {
+  _onContextCreate = async (gl, arSession) => {
 
-    const { innerWidth: width, innerHeight: height } = window;
+    const { innerWidth: width, innerHeight: height, devicePixelRatio: scale } = window;
 
     // renderer
 
     this.renderer = ExpoTHREE.createRenderer({ gl });
-    this.renderer.setPixelRatio( window.devicePixelRatio );    
+    this.renderer.setPixelRatio( scale );    
     this.renderer.setSize(width, height);
     this.renderer.setClearColor(0x000000, 1.0);
 
     // scene
 
     this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0xcccccc);
-    this.scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
+
+    // AR Background Texture
+    this.scene.background = ExpoTHREE.createARBackgroundTexture(arSession, this.renderer);
+
+    // Standard Background
+    // this.scene.background = new THREE.Color(0xcccccc);
+    // this.scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
 
     // camera
 
-    this.camera = new THREE.PerspectiveCamera(50, width / height, 1, 1000);
-    this.camera.position.z = 500;
+    /// AR Camera
+    this.camera = ExpoTHREE.createARCamera(arSession, width, height, 0.01, 1000);
+
+    /// Standard Camera
+
+    // this.camera = new THREE.PerspectiveCamera(50, width / height, 1, 1000);
+    // this.camera.position.z = 500;
+
     this.camera.lookAt(new THREE.Vector3());
 
     // controls
 
     this.controls = new OrbitControls(this.camera);
     // this.controls.addEventListener('change', this._render); // remove when using animation loop
-
-    // world
-
-    const geometry = new THREE.CylinderGeometry(0, 10, 30, 4, 1);
-    const material = new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true });
-
-    for (var i = 0; i < 500; i++) {
-
-      const mesh = new THREE.Mesh(geometry, material);
-      mesh.position.x = (Math.random() - 0.5) * 1000;
-      mesh.position.y = (Math.random() - 0.5) * 1000;
-      mesh.position.z = (Math.random() - 0.5) * 1000;
-      mesh.updateMatrix();
-      mesh.matrixAutoUpdate = false;
-      this.scene.add(mesh);
-
-    }
 
     // lights
 
@@ -82,20 +76,60 @@ class App extends React.Component {
     // resize listener
 
     window.addEventListener('resize', this._onWindowResize, false);
+
+    // setup custom world
+
+    this._setupWorld();
+  }
+
+  _setupWorld = () => {
+
+     // Rotating cube
+
+     this.cube = new THREE.Mesh(
+      new THREE.BoxGeometry(0.07, 0.07, 0.07),
+      new THREE.MeshBasicMaterial({
+        map: await ExpoTHREE.createTextureAsync({
+          asset: Expo.Asset.fromModule(require('./assets/icons/app-icon.png')),
+        }),
+      })
+    );
+    this.cube.position.z = -0.4;
+    this.scene.add(this.cube);
+
+
+    // Random Items
+
+    const geometry = new THREE.CylinderGeometry(0, 10, 30, 4, 1);
+    const material = new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true });
+
+    for (var i = 0; i < 500; i++) {
+      const mesh = new THREE.Mesh(geometry, material);
+      mesh.position.x = (Math.random() - 0.5) * 1000;
+      mesh.position.y = (Math.random() - 0.5) * 1000;
+      mesh.position.z = (Math.random() - 0.5) * 1000;
+      mesh.updateMatrix();
+      mesh.matrixAutoUpdate = false;
+      this.scene.add(mesh);
+    }
+
   }
 
   _onWindowResize = () => {
-    this.camera.aspect = window.innerWidth / window.innerHeight;
+    const { innerWidth: width, innerHeight: height, devicePixelRatio: scale } = window;
+    
+    this.camera.aspect = width / height;
     this.camera.updateProjectionMatrix();
-    this.renderer.setPixelRatio( window.devicePixelRatio );
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setPixelRatio( scale );
+    this.renderer.setSize(width, height);
   }
 
   _animate = (delta) => {
-    const { renderer, scene, camera } = this;
-    //const { width, height } = this.renderer.getSize();
+    // Rotate cube
+    this.cube.rotation.x += 0.07;
+    this.cube.rotation.y += 0.04;
 
-    // this.controls.update();  // required if controls.enableDamping = true, or if controls.autoRotate = true
+    // Render the scene
     this._render();
   }
 
@@ -105,4 +139,6 @@ class App extends React.Component {
 }
 
 // Wrap Touches Event Listener
-export default Touches(App);
+const TouchesComponent = Touches(App);
+
+export default TouchesComponent;
