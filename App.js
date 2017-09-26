17fa620a012ec01fa3371cf4637dbc3ab91e023b
Evan Bacon <baconbrix@gmail.com>
2017-09-26 11:22:00 -0500

    added asset store and helper functions

--- App.js
+++ App.js
@@ -10,40 +10,69 @@ import ThreeView from './ThreeView';
 window.DOMParser = require('xmldom').DOMParser;
 
 
-THREE.TextureLoader.prototype.load = function (url, onLoad, onProgress, onError) {
+const _assetStore = {
+  "INJ_iOS_VEHICLE_Batmobile_Arkham_Knight_Body_D.png": require('./model/INJ_iOS_VEHICLE_Batmobile_Arkham_Knight_Body_D.png'),
+  "INJ_iOS_VEHICLE_Batmobile_Arkham_Knight_Rim_D.png": require('./model/INJ_iOS_VEHICLE_Batmobile_Arkham_Knight_Rim_D.png'),
+  "INJ_iOS_VEHICLE_Batmobile_Arkham_Knight_Tire_D.png": require('./model/INJ_iOS_VEHICLE_Batmobile_Arkham_Knight_Tire_D.png'),
+};
+const cachedAssetStore = {
+  "INJ_iOS_VEHICLE_Batmobile_Arkham_Knight_Body_D.png": require('./model/INJ_iOS_VEHICLE_Batmobile_Arkham_Knight_Body_D.png'),
+  "INJ_iOS_VEHICLE_Batmobile_Arkham_Knight_Rim_D.png": require('./model/INJ_iOS_VEHICLE_Batmobile_Arkham_Knight_Rim_D.png'),
+  "INJ_iOS_VEHICLE_Batmobile_Arkham_Knight_Tire_D.png": require('./model/INJ_iOS_VEHICLE_Batmobile_Arkham_Knight_Tire_D.png'),
+};
 
-  var loader = new ImageLoader(this.manager);
+import arrayFromObject from './util/arrayFromObject';
+import cacheAssetsAsync from './util/cacheAssetsAsync';
+const loadAssets = async () => {
+  const images = arrayFromObject(_assetStore);
+  await cacheAssetsAsync({
+    images,
+  });
+
+  Object.keys(_assetStore).map(key => {
+    cachedAssetStore[key] = Expo.Asset.fromModule(_assetStore[key]);
+  });
+}
+
+THREE.TextureLoader.prototype.load = function (url, onLoad, onProgress, onError) {
+  const loader = new ImageLoader(this.manager);
   loader.setCrossOrigin(this.crossOrigin);
   loader.setPath(this.path);
 
   const texture = new THREE.Texture();
-
-
   texture.minFilter = THREE.LinearFilter; // Pass-through non-power-of-two
 
-  (async () => {
-    const asset = Expo.Asset.fromModule(require("./INJ_iOS_VEHICLE_Batmobile_Arkham_Knight_Body_D.png"));
+  if (cachedAssetStore.hasOwnProperty(url)) {
+    // This should already be downloaded.
+    const asset = cachedAssetStore[url];
+    console.warn("Load texture ", url);
+    (async () => {
+      // But try to download anyways...
+      if (!asset.localUri) {
+        await asset.downloadAsync();
+      }
 
-    // const asset = Expo.Asset.fromModule(require("../assets/models/stormtrooper/Stormtrooper_D.jpg"));
-    if (!asset.localUri) {
-      await asset.downloadAsync();
-    }
-    texture.image = {
-      data: asset,
-      width: asset.width,
-      height: asset.height,
-    };
-    texture.needsUpdate = true;
-    texture.isDataTexture = true; // Forces passing to `gl.texImage2D(...)` verbatim
+      texture.image = {
+        data: asset,
+        width: asset.width,
+        height: asset.height,
+      };
 
-    if (onLoad !== undefined) {
-      onLoad(texture);
-    }
-  })();
+      texture.needsUpdate = true;
+      texture.isDataTexture = true; // Forces passing to `gl.texImage2D(...)` verbatim
 
+      if (onLoad !== undefined) {
+        onLoad(texture);
+      }
+    })();
+  } else {
+    console.warn("Invalid Texture URL: Not in data store", url);
+  }
   return texture
 };
 
+
+const AR = false;
 
 import './Three';
 import './window/domElement';
@@ -98,18 +127,29 @@ const StereoEffect = function (renderer) {
 };
 
 class App extends React.Component {
-  render = () => (
-    <ThreeView
-      style={{ flex: 1 }}
-      onContextCreate={this._onContextCreate}
-      render={this._animate}
-      enableAR={true}
-    />
-  );
+  state = { assetsLoaded: false };
 
-  componentWillMount() {
+  render = () => {
+    if (!this.state.assetsLoaded) {
+      return (<Expo.AppLoading />);
+    }
+
+    return (
+      <ThreeView
+        style={{ flex: 1 }}
+        onContextCreate={this._onContextCreate}
+        render={this._animate}
+        enableAR={AR}
+      />
+    );
+  }
+  async componentWillMount() {
+    await loadAssets();
+
     StatusBar.setHidden(true);
     // Expo.ScreenOrientation.allow(Expo.ScreenOrientation.Orientation.LANDSCAPE_LEFT);
+    this.setState({ assetsLoaded: true });
+
   }
   _onContextCreate = async (gl, arSession) => {
 
@@ -130,22 +170,30 @@ class App extends React.Component {
 
     this.scene = new THREE.Scene();
 
-    // AR Background Texture
-    this.scene.background = ExpoTHREE.createARBackgroundTexture(arSession, this.renderer);
+    if (AR) {
+      // AR Background Texture
+      this.scene.background = ExpoTHREE.createARBackgroundTexture(arSession, this.renderer);
+    } else {
+      // Standard Background
+      this.scene.background = new THREE.Color(0xcccccc);
+      this.scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
+    }
 
-    // Standard Background
-    // this.scene.background = new THREE.Color(0xcccccc);
-    // this.scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
+
+
 
     // camera
 
     /// AR/VR Camera
-    this.camera = ExpoTHREE.createARCamera(arSession, width, height, 0.01, 1000);
 
-    // this.camera = new THREE.PerspectiveCamera(50, width / height, 1, 1000);
-    // this.camera.position.z = 5;
+    if (AR) {
+      this.camera = ExpoTHREE.createARCamera(arSession, width, height, 0.01, 1000);
+    } else {
+      this.camera = new THREE.PerspectiveCamera(50, width / height, 1, 1000);
+      this.camera.position.z = 10;
+      this.camera.lookAt(new THREE.Vector3());
+    }
 
-    // this.camera.lookAt(new THREE.Vector3());
 
     // const crosshair = new THREE.Mesh(
     //   new THREE.RingGeometry( 0.02, 0.04, 32 ),
@@ -160,15 +208,15 @@ class App extends React.Component {
 
     // controls
 
-    // VR Controls
-    // this.controls = new THREE.DeviceOrientationControls(this.camera);
+    if (!AR) {
+      this.controls = new THREE.DeviceOrientationControls(this.camera);
 
-    // this.controls = new OrbitControls(this.camera);
-    // this.controls.addEventListener('change', this._render); // remove when using animation loop
+      this.controls = new OrbitControls(this.camera);
+      this.controls.addEventListener('change', this._render); // remove when using animation loop
+
+    }
 
     // lights
-
-
 
     this.scene.add(new THREE.AmbientLight(0x666666, .8));
 
@@ -236,7 +284,25 @@ class App extends React.Component {
     // }
 
 
-    const asset = Expo.Asset.fromModule(require('./batmobile.dae'));
+    // const loadTexture = res => ExpoTHREE.createTextureAsync({
+    //     asset: Expo.Asset.fromModule(res),
+    //   });
+
+    // const textureData = [
+    //   loadTexture(require('./model/diffuse.png')),
+    //   loadTexture(require('./model/normal.png')),
+    //   loadTexture(require('./model/specular.png')),
+    // ] 
+    // const textures = await Promise.all(textureData);
+
+    // const material = new THREE.MeshPhongMaterial({
+    //   map: textures[0],
+    //   normalMap: textures[1],
+    //   specularMap: textures[2]
+    // });
+
+
+    const asset = Expo.Asset.fromModule(require('./model/model.dae'));
     if (!asset.localUri) {
       await asset.downloadAsync();
     }
@@ -250,85 +316,73 @@ class App extends React.Component {
       // this.mixer = new THREE.AnimationMixer(avatar);
       // const action = this.mixer.clipAction(animations[0]).play();
       this.scene.add(this.avatar);
-      this.avatar.position.y = -1;
-      const _scale = 0.25;
-      this.avatar.scale.set(_scale, _scale, _scale);
-      // var helper = new THREE.SkeletonHelper( avatar );
-      // helper.material.linewidth = 3;
-      // this.scene.add( helper );
+      // this.avatar.position.y = -1;
+      
+      // const _scale = 1;
+      // this.avatar.scale.set(_scale, _scale, _scale);
+      // this.avatar.material = material;
 
+      this.centerMesh(this.avatar);
+      
+      // this.scaleLongestSideToSize(this.avatar, 1);
+
+      var helper = new THREE.SkeletonHelper(this.avatar);
+      helper.material.linewidth = 3;
+      this.scene.add(helper);
+
+      console.warn("Model Loaded")
+    }, progress => {
+      const { type, target, currentTarget, eventPhase, bubbles, cancelable, timeStamp, isTrusted } = progress;
+      console.warn("Collada Progress", type, eventPhase, timeStamp, isTrusted);
+    }, error => {
+      console.error("Collada Error:", error);
     });
-    window.document.addEventListener('touchstart', (e) => {
 
+    window.document.addEventListener('touchstart', (e) => {
       // if (e.touches.length > 1) {
       //   if (this.avatar) {
       //     // avatar.lookAt(this.camera);
       //     this.avatar.rotation.y += Math.PI / 8
       //   }
-        
+
       // } else {
       //   this.moving = !this.moving;
       // }
 
     });
-
-    // const scale = 0.008;
-    // const loader = new THREE.OBJLoader();
-    // let _model;
-    // try {
-
-
-    //   _model = await new Promise((resolve, reject) =>
-    //     loader.load(
-    //       Expo.Asset.fromModule(require('./Test.obj')).uri,
-    //       resolve,
-    //       () => { },
-    //       reject)
-    //   );
-    //   _model.scale.set(scale,scale,scale)
-
-    // } catch (error) {
-    //   console.error(error);
-    // }
-
-
-    // const _texture = await ExpoTHREE.createTextureAsync({
-    //   asset: Expo.Asset.fromModule(require('./1pm1.jpg')),
-    // });
-    // const castShadow = true;
-    // const receiveShadow = false;
-
-    //     _model.traverse(child => {
-    //       if (child instanceof THREE.Mesh) {
-    //         // console.warn("m", child);
-    //         child.material.map = _texture;
-    //         child.castShadow = castShadow;
-    //         child.receiveShadow = receiveShadow;
-    //       }
-    //     });
-    //     // _model.scale.set(10, 10, 10)
-
-    //     _model.castShadow = castShadow;
-    //     _model.receiveShadow = receiveShadow;
-
-    // this.scene.add(_model);
-    // _model.position.x = 1;
-
-    // const material = new THREE.MeshLambertMaterial({ map: _texture });
-    // material.opacity = 0;
-    // const floor = new THREE.Mesh(new THREE.PlaneGeometry(400, 3500), material);
-    // floor.rotation.x = Math.PI * 1.5;
-    // floor.receiveShadow = true;
-    // // this.scene.add(floor);
-    // this.scene.add(_model);
+    this.addGrid({
+      meters: 10,
+      divisions: 20
+    });
 
     // this.scene.add(new THREE.GridHelper(10, 10 * 3.28084));
   }
 
-  getWidth = (mesh) => {
-    var box = new THREE.Box3().setFromObject(mesh);
-    // console.log( box.min, box.max, box.size() );
-    return box.size().width;
+  addGrid = ({ meters, divisions }) => {
+    if (this.scene && meters && divisions) {
+      this.scene.add(new THREE.GridHelper(meters, divisions));
+    }
+  }
+
+  centerMesh = (mesh) => {
+    const box = new THREE.Box3().setFromObject(mesh);
+    const size = box.size();
+    const { x, y, z } = box.min;
+    const min = {x: -x, y: -y, z: -z};
+    const half = {width: size.x / 2, height: size.y / 2, depth: size.z / 2 };
+    mesh.position.set(min.x - half.width, min.y - half.height, min.z - half.depth);
+  }
+  scaleLongestSideToSize = (mesh, size) => {
+    const { x: width, y: height, z: depth } = this.getSize(mesh);
+    const longest = Math.max(width, Math.max(height, depth));
+    const scale = size / longest;
+    mesh.scale.set(scale, scale, scale);
+  }
+
+  getSize = (mesh) => {
+    const box = new THREE.Box3().setFromObject(mesh);
+    console.warn("size", box.min, box.max, box.size());
+    return box.size();
   }
 
   _onWindowResize = () => {
